f51ca37ce64ad72ea879ddb3a46f8c629c8502cd
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-06-29 17:27:34 +0000

    edit user model

---
+++ FineManagement.Api/FineManagement.Infrastructure/Migrations/20220629171625_addIsRepresentativeOfTeamField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FineManagement.Infrastructure.Migrations
+{
+    public partial class addIsRepresentativeOfTeamField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRepresentativeOfTeam",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRepresentativeOfTeam",
+                table: "Users");
+        }
+    }
+}
